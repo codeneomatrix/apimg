d004a366e32d14206a211ded2770c4d6f2b79fd7
neomatrix <josuecevedo@gmail.com>
2016-02-20 00:21:21 -0600

    etiquetado.cpp

---
+++ etiquetado.cpp
@@ -0,0 +1,20 @@
+/*
+  program:1
+  Name: etiquetado.cpp
+  Author: josue acevedo maldonado
+  Date: 20/02/2016 
+  Description:
+*/
+#include <stdio.h>
+#include <stdlib.h>
+//#include <math.h>
+
+
+int main(){	
+
+    printf("Algoritmo de etiquetado \n");
+    
+
+    //system("pause");
+    return 0;
+    }
