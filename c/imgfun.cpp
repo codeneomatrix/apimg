673337ea76069f5da0a9ec2c578b438b83c440fb
neomatrix <josuecevedo@gmail.com>
2016-03-01 06:14:34 -0600

    refactorizando eliminando la memoria dinamica

--- c/imgfun.cpp
+++ c/imgfun.cpp
@@ -19,96 +19,94 @@ int matrix2[9]= {1,0,0,
 			   	 2,2,2};
 
 
-void imprimir(int m[],int l){
+void imprimir(int m[],int l,int h){
 	for(int i=0; i<l; i++){
-		printf("%d",m[i]);
+		for(int j=0; j<h; j++){
+			printf("\t%d ",m[j+(i*l)]);
+		}
+		printf("\n");
 	}
 	printf("\n");
 }
 
+void reset(int m[],int l){
+	for(int i=0; i<l; i++){
+		m[i]=0;
+	}
+}
+
 /*funcion de etiquetado*/
-int* etiquetado(int x[] ){
+void etiquetado(int s[],int x[] ){
 	printf("Algoritmo de etiquetado \n");
-	imprimir(x,9);
-	return matrix2;
+	imprimir(x,4,4);
+
 }
 
 	/**Algoritmo para convertir imagen COLOR a GRAY**/
-int* grises(int r[],int g[],int b[],int alto,int ancho){
+void grises(int s[],int r[],int g[],int b[],int alto,int ancho){
 		printf("Algoritmo de Grises\n");
-		int* aux;//auxiliar que regresa la funcion
+		printf("\ntamaño: %d\n",sizeof(r));
+		
 		for (size_t i = 0; i < ancho*alto; i++) {
 			int val=(r[i]+g[i]+b[i])/3; //calculo del promedio de los canales RGB
-			aux[i]=val;//asignacion al vector de retorno del promedio
+			s[i]=val;//asignacion al vector de retorno del promedio
 		}
-	return aux; //regreso del auxiliar
 }
 /**Algoritmo para umbralado**/
-int * umbral(int x[],int umbral,int alto,int ancho){
-	printf("Algoritmo de Grises\n");
-	int* aux = (int*)malloc(ancho*alto*(sizeof(int)));//auxiliar que regresa la funcion
+void umbral(int s[], int x[],int umbral,int alto,int ancho){
+	printf("Algoritmo de umbralado\n");
 	for (size_t i = 0; i < ancho*alto; i++) {
 		if(x[i]<=umbral){
-			aux[i]=0;
+			s[i]=0;
 		}
 		else{
-			aux[i]=255;
+			s[i]=255;
 		}
 
 	}
-return aux; //regreso del auxiliar
 
 }
 
 int* canny(int x[] ){
 	printf("Algoritmo de canny \n");
-	imprimir(x,9);
+	imprimir(x,4,4);
 	return matrix2;
 }
 
 int* sobel(int x[] ){
 	printf("Algoritmo de sobel \n");
-	imprimir(x,9);
+	imprimir(x,4,4);
 	return matrix2;
 }
-int * convolucion(int mascara[],int img[],int alto,int ancho){
-	int masc[3][3];
-	int ind=0;
-	for (size_t i = 0; i <3; i++) {
-		for (size_t j =0; j<3; j++) {
-			masc[i][j]=mascara[ind];
-			printf("%d",masc[i][j]);
-			ind++;
-		}
-		printf("\n");
+
+double producto_escalar(int v1[], int v2[], int d){
+	// vector_1[] = {5,1,0};
+	// vector_2[] = {-1,5,3};
+	
+	int resultado = 0;
+	int i;
+	for (i=0; i < d; i++) {
+		resultado += v1[i] * v2[i];
 	}
-	int **matriz;
-
-matriz = (int **)malloc (alto*sizeof(int *));
-mataux =(int **)malloc (alto*sizeof(int *));
-for (int i=0;i<alto;i++){
-	matriz[i] = (int *) malloc (ancho*sizeof(int));
-	mataux[i] = (int *) malloc (ancho*sizeof(int));
+	return resultado;
 }
-	ind=0;
-	for (size_t i = 0; i < alto; i++) {
-		for (size_t j = 0; j < ancho; j++) {
-			matriz[i][j]=img[ind];
-			printf("%d",matriz[i][j]);
-			ind++;
+
+void convolucion(int salida[], int mascara[],int altom,int anchom,int img[],int alto,int ancho){
+	printf("Algoritmo de convolucion \n");
+	
+	int tem[altom*anchom];
+	
+	//primer PARCHE
+	for(int i=0; i<altom; i++){
+		for(int j=0; j<anchom; j++){
+			tem[j+(i*alto)]=img[j+(i*alto)];
 		}
-		printf("\n");
 	}
-for (size_t i = 1; i < alto; i++) {
-		for (size_t j = 1; j <ancho; j++) {
-			suma=(matriz[i-1][j-1]*masc[0][0])+(matriz[i-1][j]*masc[0][1])+
-			(matriz[i+1][j-1]*masc[0][2])+(matriz[i][j-1]*masc[1][0])+
-			(matriz[i][j+1]*masc[1][2])+(matriz[i+1][j-1]*masc[2][0])+
-			(matriz[i+1][j]*masc[2][1])+(matriz[i+1][j+1]*masc[2][2]);
-			mataux[i][j]=suma;
-		}
+	
+	int r = producto_escalar(tem,mascara,(altom*anchom));
+	printf("\n r= %d \n",r);
+	
+printf("\ntamaño mascara : %d\n",sizeof(mascara));
 }
 
 
-
-}
--- c/main.cpp
+++ c/main.cpp
@@ -8,40 +8,48 @@
 #include "imgfun.cpp"
 #include <vector>
 
-int* sa;
-int matrix[9]= {10,5,3,
-								2,4,0,
-								3,9,8};
-int matrixr[9]= {10,6,9,
-								8,9,7,
-								4,6,2};
-int matrixd[9]= {5,5,6,
-								4,7,8,
-								1,2,9};
+int sa[9];
+int matrixsalida[16];
+int kernel1[9]={1,2,3,
+				5,7,5,
+				2,2,9};
+				
+int matrix[16]= {10,5,3,7,
+				 2,4,0,1,
+				 4,6,2,3,
+				 3,9,8,3};
+int matrixr[16]= {10,6,9,7,
+				  8,9,7,8,
+				  1,6,5,2,
+				  4,6,2,6};
+int matrixd[16]= {5,5,6,3,
+				 4,7,8,2,
+				 1,6,2,7,
+				 1,2,9,4};
 
 int main(){
 
-    sa= etiquetado(matrix);
+    //sa= etiquetado(matrix);
 
-	imprimir(sa,9);
+	//imprimir(sa,9);
 
-	sa= grises(matrix,matrixr,matrixd,3,3);
+	grises(matrixsalida,matrix,matrixr,matrixd,4,4);
 
-	imprimir(sa,9);
+	imprimir(matrixsalida,4,4);
 
-	sa=umbral(matrixr,5,3,3);
+	umbral(matrixsalida,matrixr,5,4,4);
 
-	imprimir(sa,9);
+	imprimir(matrixsalida,4,4);
 
-	sa= canny(matrix);
+	//sa= canny(matrix);
 
-	imprimir(sa,9);
+	//imprimir(sa,9);
 
-	sa= sobel(matrix);
+	//sa= sobel(matrix);
 
-	imprimir(sa,9);
+	//imprimir(sa,9);
 
-	sa=convolucion(matrix,matrixr,3,3);
+	convolucion(matrixsalida,kernel1,3,3,matrixr,4,4);
 
     return 0;
     }
