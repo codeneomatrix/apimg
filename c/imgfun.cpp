7c30bf1aad28f58e20ec0a6db75fad97a2de115f
subzeroDevelopment <subcero000@gmail.com>
2016-03-02 16:42:34 -0600

    convolucion

--- c/imgfun.cpp
+++ c/imgfun.cpp
@@ -9,15 +9,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
-
-
+#include <math.h>
 //#include <vector>
-//#include <math.h>
 
 int matrix2[9]= {1,0,0,
 				 0,2,0,
 			   	 2,2,2};
 
+
+void imprimirvector(int m[],int l,int h){
+	for(int i=0; i<l*h; i++){
+			printf(" %d ",m[i]);
+	}
+	printf("\n");
+}
 
 void imprimir(int m[],int l,int h){
 	for(int i=0; i<l; i++){
@@ -45,8 +50,8 @@ void etiquetado(int s[],int x[] ){
 	/**Algoritmo para convertir imagen COLOR a GRAY**/
 void grises(int s[],int r[],int g[],int b[],int alto,int ancho){
 		printf("Algoritmo de Grises\n");
-		printf("\ntamaño: %d\n",sizeof(r));
-		
+		printf("\ntamaï¿½o: %d\n",sizeof(r));
+
 		for (size_t i = 0; i < ancho*alto; i++) {
 			int val=(r[i]+g[i]+b[i])/3; //calculo del promedio de los canales RGB
 			s[i]=val;//asignacion al vector de retorno del promedio
@@ -82,7 +87,7 @@ int* sobel(int x[] ){
 double producto_escalar(int v1[], int v2[], int d){
 	// vector_1[] = {5,1,0};
 	// vector_2[] = {-1,5,3};
-	
+
 	int resultado = 0;
 	int i;
 	for (i=0; i < d; i++) {
@@ -93,20 +98,40 @@ double producto_escalar(int v1[], int v2[], int d){
 
 void convolucion(int salida[], int mascara[],int altom,int anchom,int img[],int alto,int ancho){
 	printf("Algoritmo de convolucion \n");
-	
+	reset(salida,alto);
+
+ int centroh=ceil((double)(anchom/2))-1;
+ int centrov=ceil((double)(altom/2))-1;
+
 	int tem[altom*anchom];
-	
-	//primer PARCHE
-	for(int i=0; i<altom; i++){
-		for(int j=0; j<anchom; j++){
-			tem[j+(i*alto)]=img[j+(i*alto)];
+int mv=0;
+int mh=0;
+int n=alto-2;
+for(int k=0;k<n;k++){
+	mv=k;
+	for (int l = 0;l<n;l++) {
+		mh=l;
+		int con=0;
+		for(int i=(0+mv); i<(altom+mv); i++){
+			for(int j=(0+mh); j<(anchom+mh); j++){
+				//printf("tem[%d+(%d*%d)]=img[%d+(%d*%d)=%d ==> %d]\n",j,i,alto,j,i,alto,j+(i*alto),img[j+(i*alto)]);
+				tem[con]=img[j+(i*alto)];
+				con++;
+			}
 		}
+
+		//printf("%d\n",(altom*anchom));
+		//imprimir(tem,altom,anchom);
+		//imprimir(mascara,altom,anchom);
+		int r = producto_escalar(tem,mascara,(altom*anchom));
+		//printf("%d\n", centrov+mv);
+		salida[(centroh+mh)+((centrov+mv)*alto)]=r;
+		//printf("\n r= %d \n",r);
+
 	}
-	
-	int r = producto_escalar(tem,mascara,(altom*anchom));
-	printf("\n r= %d \n",r);
-	
-printf("\ntamaño mascara : %d\n",sizeof(mascara));
+	//primer PARCHE
+	}
+
+	imprimir(salida,alto,ancho);
+//printf("\ntamaÃ±o mascara : %d\n",sizeof(mascara));
 }
-
-
